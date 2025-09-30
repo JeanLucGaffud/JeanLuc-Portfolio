5e20a726657b0d1fcdbc18b53fb7a25cdfcf56ab
Jean Luc <149998953+Jeyshel@users.noreply.github.com>
2025-09-30 14:49:37 +0800

    Update auth-client.ts

--- frontend/src/lib/auth-client.ts
+++ frontend/src/lib/auth-client.ts
@@ -1,7 +1,7 @@
 import { createAuthClient } from "better-auth/react"
 export const authClient = createAuthClient({
     /** The base URL of the server (optional if you're using the same domain) */
-    baseURL: "http://localhost:3000"
+    baseURL: "https://jean-luc-portfolio.vercel.app/"
 })
 
 export const {signUp, useSession, signOut } = authClient
